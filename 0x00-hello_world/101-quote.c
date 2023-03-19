4dedb409057c8cc3eae5a381825ff57e61bb205f
frank <kamunyuz@gmail.com>
2023-03-19 00:46:48 -0700

    Prints to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Prints a text to the standard output error
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1);
-	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
