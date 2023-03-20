889a9b0cf7f079727fcd41b5128b872da77a7ed8
frank <kamunyuz@gmail.com>
2023-03-20 15:57:05 -0700

    Reverse mode

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ *main - Entry point
+ *Description:Prints lowercase in reverse
+ *Return: Always 0
+ */
+int main(void)
+{
+	int a = 122;
+
+	while (a >= 97)
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -5,3 +5,4 @@ Prints lowercase then uppercase
 Prints the alphabet but ommits some characters
 Prints all single digits of base 10
 Prints single digits with no variable type of char
+Prints lowecase alphabet in reverse
