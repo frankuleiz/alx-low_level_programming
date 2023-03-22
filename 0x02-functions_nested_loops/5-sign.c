676944f362661a75796c4c4c41740a981f682cc6
frank <kamunyuz@gmail.com>
2023-03-22 02:03:08 -0700

    Prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * print_sign: Entry point
+  * @n: Parameter to be executed
+  * Return: 1 0 or -1
+  */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
