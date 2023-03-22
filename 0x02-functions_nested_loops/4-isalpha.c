e35a9b69c3e4105bbfc9adc5657e269b3088e258
frank <kamunyuz@gmail.com>
2023-03-21 18:49:06 -0700

    Checks both uppercase and lower case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isalpha - Entry point
+* @c: parameter to be checked
+* Return: 1 or 0
+*/
+int _isalpha(int c)
+{
+	if (c <= 65 || c >= 90 || c <= 65 || c >= 122)
+		return (1);
+	else
+		return (0);
+}
