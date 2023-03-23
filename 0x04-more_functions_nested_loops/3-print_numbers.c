026372491118efa42fde523532261c8f012357da
frank <kamunyuz@gmail.com>
2023-03-23 03:30:46 -0700

    Print from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_numbers - Print numbers from o to 9
+  * c: parameter
+  * Return: return void
+  */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; 48 >= 57; c++)
+	{
+		_putchar(c);
+
+	}
+	_putchar('\n');
+}
