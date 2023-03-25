96742ad2e5e78f53253006519b51a16b528dd4cf
frank <kamunyuz@gmail.com>
2023-03-24 19:54:47 -0700

    ask for redo

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,1 @@
-#include "main.h"
-/**
-  * main - tests function that prints positve or negative
-  * Return: 0
-  */
-int main(void)
-{
-	int i;
 
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
-}
