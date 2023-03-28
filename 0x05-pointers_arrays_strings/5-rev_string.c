537e984a19bdad04326b0904215db372c06f6b11
frank <kamunyuz@gmail.com>
2023-03-27 19:07:38 -0700

    A good engineer thinks in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * rev_string - reverses a string
+  * @s: character to be reversed
+  * Return: void
+  */
+void rev_string(char *s)
+{
+	int a = 0;
+	int b;
+	int c;
+	char d;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	c = a - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
+	}
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -4,3 +4,5 @@
 1. A function that swaps the values of two integers
 2. A function that returns the length of a string
 3. A function that prints a string, followed by a new line, to stdout
+A function that prints a string, in reverse, followed by a new line
+5. A function that reverses a string
