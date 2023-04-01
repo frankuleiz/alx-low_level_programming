8d321168e3d7d315d4871a2bc0d4307e35c34dff
frank <kamunyuz@gmail.com>
2023-03-31 18:37:46 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * string_toupper - Entry point
+  * @a: pointer
+  * Return: n
+  */
+char *string_toupper(char *a)
+{
+	int i;
+
+	i = 0;
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] = a[i] - 32;
+		i++;
+	}
+	return (a);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -4,3 +4,4 @@
 2. A function that copies a string
 3. A function that compares two strings
 4. A function that reverses the content of an array
+5. A function that changes all from lowercase letters to uppercase of a string
