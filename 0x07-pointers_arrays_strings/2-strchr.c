a7f0d1eeb99b7e67ffca197d8580f40701ccdf24
frank <kamunyuz@gmail.com>
2023-04-03 15:46:55 -0700

    strch

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _strchr - Entry point
+  * @s: string
+  * @c: first occurence character
+  * Return: s or NULL
+  */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i= 0; i >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
