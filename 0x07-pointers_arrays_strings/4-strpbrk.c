3c460ad9900c90e759f132090db7e33479173630
frank <kamunyuz@gmail.com>
2023-04-03 17:01:32 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *_strpbrk - searches a string for any set of bytes
+  * @s: charcter
+  * @accept: string
+  * Return: accept or null
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (0);
+}
