3538aa0fcdf451b33e4063d6d1e13e53b70afbdb
frank <kamunyuz@gmail.com>
2023-04-06 16:29:32 -0700

    power of x to y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _pow_recursion - raise x to the power of y
+  * @x: base int
+  * @y: exponent
+  * Return: always 0
+  */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -3,3 +3,4 @@
 1. A function that prints a string in reverse
 2. A  function that returns the length of a string
 3. A function that returns the factorial of a given number
+4. A  function that returns the value of x raised to the power of y
