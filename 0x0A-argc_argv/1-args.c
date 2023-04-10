ee3bc143dab047ac791bede526791e5e6ad8e745
frank <kamunyuz@gmail.com>
2023-04-10 14:25:35 -0700

    Prints the number of arguements

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - prints the number of arguements
+  * @argc: number of arguements
+  * @argv: array of arguements
+  * Return: Always 0
+  */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
