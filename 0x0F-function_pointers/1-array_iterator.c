22c7d12a00db002da7bfc8362e61907029f6d983
frank <kamunyuz@gmail.com>
2023-04-19 08:56:44 -0700

    fuction that executes a function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if (array == NULL && action == NULL)
+	if (array == NULL || action == NULL)
 		return;
-	for (i = 0; i < (int)size; i++)
+
+	for (i = 0; i < (int) size; i++)
 	{
 		action(array[i]);
 	}
