a7d5e0ada24a20e828462e8a4c2904c6c2c55139
Francis <kamunyuz@gmail.com>
2023-04-27 13:15:00 -0700

    advance task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_first(void)__attribute__((constructor));
+
+/**
+  * print_first - prints a sentence before main
+  * Return: void
+  */
+
+void print_first(void)
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
