a3b1405cce69af948df4d45ff8dc9d6ec40753cd
Francis <kamunyuz@gmail.com>
2023-05-01 03:10:29 -0700

    prints all elements

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,8 +15,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
@@ -30,6 +30,6 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_ts **head, unsigned int index);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif
