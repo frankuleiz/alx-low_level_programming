5fdc43d2ea3e4a7f57adb6f7eee09c50e6a92e60
Francis <kamunyuz@gmail.com>
2023-05-01 02:42:31 -0700

    prints all the elements

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,15 +10,14 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structur
+ * Description: singly linked list node structure
  *
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
-
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
