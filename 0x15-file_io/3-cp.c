87a76cf659ae547432c7f75b6b6435899ad85141
Frank <kamunyuz@gmail.com>
2023-07-14 15:23:47 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int src, dest, r, x, y;
-	char buf[SIZ];
+	char buf[BUFSIZ];
 
 	if (argc != 3)
 	{
@@ -20,22 +20,22 @@ int main(int argc, char *argv[])
 	src = open(argv[1], O_RDONLY);
 	if (src < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %S\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((r = read(src, buf, SIZ)) > 0)
+	while ((r = read(src, buf, BUFSIZ)) > 0)
 	{
-		if (dest < 0 || write(dst, buf, r) != r)
+		if (dest < 0 || write(dest, buf, r) != r)
 		{
-			dprint(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(src);
 			exit(99);
 		}
 	}
 	if (r < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %S\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	x = close(src);
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 	if (x < 0 || y < 0)
 	{
 		if (x < 0)
-			dprintf(STDER_FILENO, "Error: Can't close fd %d\n", src);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
 		if (y < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest);
 		exit(100);
