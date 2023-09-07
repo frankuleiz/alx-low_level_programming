ed033f2c987c55c664d8183bd3dbbac34020e702
Frank <kamunyuz@gmail.com>
2023-09-07 15:51:30 +0300

    betty correctiuons

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,21 +7,21 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
-    unsigned long int i;
+	hash_table_t *ht;
+	unsigned long int i;
 
-    ht = malloc(size * sizeof(hash_table_t));
-        if (ht == NULL)
-        return (NULL)
-        ht->size = size;
-    ht->array = malloc(size * sizeof(hash_node_t*));
-    if (ht-array == NULL)
-    {
-        free(ht);
-        return (NULL);
-    }
-    for (i = 0, i < ht->size; i++)
-        ht->array[i] = NULL;
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_node_t *));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-    return (ht);
+	return (ht);
 }
